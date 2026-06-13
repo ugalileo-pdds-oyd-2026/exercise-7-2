b4ca5462415a3f2a8ab2aa83b1c0ecefadbe55d8
Tito Alvarez <augustoalvarez@gmail.com>
2026-06-12 21:21:27 -0600

    add comment to main.tf to trigger PR checks

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,4 @@
+# SQS queue for Exercise 7.2 verification
 resource "aws_sqs_queue" "main" {
   name                       = var.queue_name
   visibility_timeout_seconds = var.visibility_timeout_seconds
